3a478b6c734df310e6d0652d59ef5886dba4c748
Anush Dubey <91270256+Anushdubey01@users.noreply.github.com>
2023-05-11 23:04:46 +0530

    Add files via upload

---
+++ 1822. Sign of the Product of an Array/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int res = 1;
+        for (int num : nums) {
+            if (num > 0) continue;
+            else if (num == 0) return 0;
+            else res *= -1;
+        }
+        return res;
+    }
+}
