32699e9b42ca754c3dfa401c4134a24e2683575f
Anush Dubey <91270256+Anushdubey01@users.noreply.github.com>
2023-05-26 22:06:07 +0530

    Add files via upload

---
+++ 1140. Stone Game II/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+  public int stoneGameII(int[] piles) {
+    final int n = piles.length;
+    dp = new int[n][n];
+    suffixSum = piles.clone();
+
+    for (int i = n - 2; i >= 0; --i)
+      suffixSum[i] += suffixSum[i + 1];
+
+    return stoneGameII(0, 1);
+  }
+
+  private int[][] dp;
+  private int[] suffixSum;
+
+  private int stoneGameII(int i, int M) {
+    if (i + 2 * M >= dp.length)
+      return suffixSum[i];
+    if (dp[i][M] > 0)
+      return dp[i][M];
+
+    int opponent = suffixSum[i];
+
+    for (int X = 1; X <= 2 * M; ++X)
+      opponent = Math.min(opponent, stoneGameII(i + X, Math.max(M, X)));
+
+    return dp[i][M] = suffixSum[i] - opponent;
+  }
+}
