20bb23b3cae48828421d2b2b90112fd9ec85a63f
Anush Dubey <91270256+Anushdubey01@users.noreply.github.com>
2023-05-30 20:56:02 +0530

    Add files via upload

---
+++ 705. Design HashSet/Solution.java
@@ -0,0 +1,19 @@
+class MyHashSet {
+  public MyHashSet() {
+    set = new boolean[1000001];
+  }
+
+  public void add(int key) {
+    set[key] = true;
+  }
+
+  public void remove(int key) {
+    set[key] = false;
+  }
+  public boolean contains(int key) {
+    return set[key];
+  }
+
+  private boolean[] set = new boolean[1000001];
+}
+
