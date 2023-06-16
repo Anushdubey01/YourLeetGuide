d10d0dd9c87559ffef917bd8247366be01c868c0
Anush Dubey <91270256+Anushdubey01@users.noreply.github.com>
2023-06-16 21:54:28 +0530

    Solution.py

---
+++ 1569. Number of Ways to Reorder Array to Get Same BST/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numOfWays(self, nums: List[int]) -> int:
+        def f(nums):
+            if len(nums) <= 1:
+                return 1
+            else:
+                r = [n for n in nums[1:] if n > nums[0]]
+                l = [n for n in nums[1:] if n < nums[0]]
+                return comb(len(r)+len(l), len(r))*f(l)*f(r)
+        return (f(nums)-1) % (10**9 + 7)
