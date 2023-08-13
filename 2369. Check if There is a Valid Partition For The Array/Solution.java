03aaf85d0c44db6fc1867b8ac544e827556b12c0
Anush Dubey <91270256+Anushdubey01@users.noreply.github.com>
2023-08-13 10:19:41 +0530

    Add files via upload

---
+++ 2369. Check if There is a Valid Partition For The Array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean validPartition(int[] nums) {
+        final int n = nums.length;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        dp[2] = nums[0] == nums[1];
+        for (int i = 3; i <= n; ++i) {
+            dp[i] = (dp[i - 2] && nums[i - 2] == nums[i - 1]) ||
+                    (dp[i - 3] && ((nums[i - 3] == nums[i - 2] && nums[i - 2] == nums[i - 1]) ||
+                                   (nums[i - 3] + 1 == nums[i - 2] && nums[i - 2] + 1 == nums[i - 1])));
+        }
+        return dp[n];
+    }
+}
